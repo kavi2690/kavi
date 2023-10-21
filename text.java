daa3458497557406daf9ae44bcbda0ac40ccd262
kavi2690 <148617150+kavi2690@users.noreply.github.com>
2023-10-21 16:51:52 +0530

    text
    text and texbox

--- text.java
+++ text.java
@@ -7,29 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class text {
 	
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
-		ChromeDriver c= new ChromeDriver();
 		
-		c.get("https://letcode.in/selenium");
+			System.setProperty("webdriver.chrome.driver", "C:\\Users\\\\Admin\\Downloads\\chromedriver-win64\\chromedriver.exe");
+			ChromeDriver c= new ChromeDriver();
+			
+			c.get("https:www.facebook.com");
+			
+			c.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			
+			c.manage().window().maximize();
+			
+			c.findElementByName("email").sendKeys("6380606117");
+			
+			Thread.sleep(4000);
+			
+			c.findElementByName("pass").sendKeys("kavi2426");
+			
+			c.findElementByName("login").click();
+			
+			c.quit();
 		
-		c.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
-		c.manage().window().maximize();
-		
-	   c.findElementByLinkText("Sign up").click();
-	   
-	   Thread.sleep(4000);
-	   
-	  c.navigate().back();
-	  
-	  Thread.sleep(4000);
-	  
-	  c.navigate().forward();
-	  
-	  Thread.sleep(4000);
-	  
-	  c.navigate().refresh();
-	  
-	  c.quit();}
-
-}
+			
+		  // c.findElementByLinkText("Sign up").click();
+}}
